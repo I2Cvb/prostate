71e3c378821e93e345cacd959804aaf46b43cf0b
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-13 01:19:09 +0100

    Finish to modify the resampling method using ITK

--- src/resampling/resampling_dce_from_t2w.cxx
+++ src/resampling/resampling_dce_from_t2w.cxx
@@ -1,9 +1,9 @@
 /*!
- * \file dicom_read_write.cxx
- * \brief This code read a serie of DICOM images and save it back. It is a routine to enforce the consistency of the coordinate system.
+ * \file resampling_dce_from_t2w.cxx
+ * \brief Pipeline to resample DCE image from T2W image and save DICOM accordingly.
  * \author Guillaume Lemaitre - LE2I ViCOROB
  * \version 0.1
- * \date March, 2014
+ * \date March, 2016
  */
 
 #include "itkVersion.h"
@@ -94,10 +94,12 @@ int main( int argc, char* argv[] )
 	InterpolatorType;
     typedef itk::ResampleImageFilter< InputImageType, InputImageType >
 	ResampleFilterType;
-    typedef itk::ShiftScaleImageFilter< InputImageType, InputImageType >
-	ShiftScaleType;
     typedef itk::ImageSeriesWriter< InputImageType, OutputImageType >
 	SeriesWriterType;
+    typedef itk::StatisticsImageFilter< OutputImageType > 
+        StatisticsImageFilterType;
+    typedef itk::ExtractImageFilter< InputImageType, OutputImageType > 
+        ExtractFilterType;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // READ THE T2W IMAGE TO USE THE SPATIAL INFORMATION FOR THE RESAMPLING
@@ -105,17 +107,17 @@ int main( int argc, char* argv[] )
 
     // List the file inside the directory
     InputNamesGeneratorType::Pointer t2w_filenames_generator = InputNamesGeneratorType::New();
-    t2w_filenames_generator->SetInputDirectory( argv[1] );
+    t2w_filenames_generator->SetInputDirectory(argv[1]);
 
     // Get the filename corresponding to the first serie
     const ReaderType::FileNamesContainer& t2w_filenames = 
-	t2w_filenames_generator->GetFileNames( t2w_filenames_generator->GetSeriesUIDs().begin()->c_str() );
+	t2w_filenames_generator->GetFileNames(t2w_filenames_generator->GetSeriesUIDs().begin()->c_str());
 
     // Load the volume inside the image object
     ImageIOType::Pointer gdcm_t2w = ImageIOType::New();
     ReaderType::Pointer t2w_volume = ReaderType::New();
-    t2w_volume->SetImageIO( gdcm_t2w );
-    t2w_volume->SetFileNames( t2w_filenamesOD );
+    t2w_volume->SetImageIO(gdcm_t2w);
+    t2w_volume->SetFileNames(t2w_filenames);
 
     // Try to update to catch up any error
     try {
@@ -128,13 +130,13 @@ int main( int argc, char* argv[] )
     }
 
     // Let's display some information about the DICOM
-    std::cout << "The filename of the T2W serie are:" << std::endl;
-    for(auto it = inputAnnotatedFilenames.begin(); it != inputAnnotatedFilenames.end(); ++it)
+    std::cout << "The filenames of the T2W serie are:" << std::endl;
+    for(auto it = t2w_filenames.begin(); it != t2w_filenames.end(); ++it)
 	std::cout << *it << std::endl;
     std::cout << "" << std::endl;
     std::cout << "******************************" << std::endl;
     std::cout << "" << std::endl;
-    std::cout << "Information about the annotated DICOM volume:" << std::endl;
+    std::cout << "Information about the T2W DICOM volume:" << std::endl;
     std::cout << "Spacing: " << t2w_volume->GetOutput()->GetSpacing() << std::endl;
     std::cout << "Origin:" << t2w_volume->GetOutput()->GetOrigin() << std::endl;
     std::cout << "Direction:" << std::endl << t2w_volume->GetOutput()->GetDirection() << std::endl;
@@ -147,37 +149,33 @@ int main( int argc, char* argv[] )
     // READ THE DCE IMAGES FROM THE DIFFERENT SERIES
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    /*---- Read the original data ----*/
     // Variable to generate the list of files
-    InputNamesGeneratorType::Pointer inputFilenamesGenerator = InputNamesGeneratorType::New();
-    inputFilenamesGenerator->SetInputDirectory( argv[1] );
+    InputNamesGeneratorType::Pointer dce_filenames_generator = InputNamesGeneratorType::New();
+    dce_filenames_generator->SetInputDirectory(argv[2]);
 
     // Check the number of series that are available
-    const itk::SerieUIDContainer& serieContainer = inputFilenamesGenerator->GetSeriesUIDs();
-    std::cout << "The number of series present is: " << serieContainer.size() << std::endl;
+    const itk::SerieUIDContainer& dce_serieuid = dce_filenames_generator->GetSeriesUIDs();
+    std::cout << "The number of series in the DCE acquisition: " << dce_serieuid.size() << std::endl;
 
     // Sort the vector of serie to be in proper order
-    std::sort(serieContainer.begin(), serieContainer.end(), comparisonSerieID);
+    std::sort(dce_serieuid.begin(), dce_serieuid.end(), comparisonSerieID);
     
     unsigned int nFile = 0;
-    for ( unsigned int nSerie = 0 ; nSerie < serieContainer.size() ; nSerie++ ) {
-	// Container with the different filenames
-	const ReaderType::FileNamesContainer& inputFilenames = 
-	    inputFilenamesGenerator->GetFileNames( serieContainer[nSerie] );
+    for (unsigned int nSerie = 0; nSerie < dce_serieuid.size(); ++nSerie) {	
+	
+        // Container with the different filenames
+	const ReaderType::FileNamesContainer& dce_filenames = 
+	    dce_filenames_generator->GetFileNames(dce_serieuid[nSerie]);
       
-	std::cout << "Serie UID: " << serieContainer[nSerie] << std::endl;
-	for(auto it = inputFilenames.begin(); it != inputFilenames.end(); ++it)
-	    std::cout << *it << std::endl;
-
 	// Reader corresponding to the actual mask volume 
-	ImageIOType::Pointer gdcmIO = ImageIOType::New();
-	ReaderType::Pointer inputVolume = ReaderType::New();
-	inputVolume->SetImageIO( gdcmIO );
-	inputVolume->SetFileNames( inputFilenames );
+	ImageIOType::Pointer gdcm_dce = ImageIOType::New();
+	ReaderType::Pointer dce_volume = ReaderType::New();
+	dce_volume->SetImageIO(gdcm_dce);
+        dce_volume->SetFileNames(dce_filenames);
 
 	// Try to update to catch up any error
 	try {
-	    inputVolume->Update();
+	    dce_volume->Update();
 	}
 	catch (itk::ExceptionObject &excp) {
 	    std::cerr << "Exception thrown while reading the series" << std::endl;
@@ -186,16 +184,24 @@ int main( int argc, char* argv[] )
 	}
 
 	// Let's display some information about the DICOM
-	std::cout << "Information about the mask volume:" << std::endl;
-	std::cout << "Spacing: " << inputVolume->GetOutput()->GetSpacing() << std::endl; 
-	std::cout << "Origin:" << inputVolume->GetOutput()->GetOrigin() << std::endl;
-	std::cout << "Direction:" << std::endl << inputVolume->GetOutput()->GetDirection() << std::endl;  
-	std::cout << "Size:" << inputVolume->GetOutput()->GetLargestPossibleRegion().GetSize() << std::endl;
+	std::cout << "The filenames of the DCE serie are:" << std::endl;
+	for(auto it = dce_filenames.begin(); it != dce_filenames.end(); ++it)
+	    std::cout << *it << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "******************************" << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "Information about the DCE DICOM volume:" << std::endl;
+	std::cout << "Spacing: " << dce_volume->GetOutput()->GetSpacing() << std::endl;
+	std::cout << "Origin:" << dce_volume->GetOutput()->GetOrigin() << std::endl;
+	std::cout << "Direction:" << std::endl << dce_volume->GetOutput()->GetDirection() << std::endl;
+	std::cout << "Size:" << dce_volume->GetOutput()->GetLargestPossibleRegion().GetSize() << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "******************************" << std::endl;
+	std::cout << "" << std::endl;
 
-	// Resample the volume now
-	/* ----------------------------------------------------------------------*/
-	/*----- Map the annotated volume to the serie of the volume to map ------*/
-	/* ----------------------------------------------------------------------*/
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// RESAMPLE THE DCE VOLUME USING THE INFORMATION OF THE T2W VOLUME
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// Declare which interpolation and transform are needed
 	InterpolatorType::Pointer interpolator = InterpolatorType::New();
@@ -204,18 +210,19 @@ int main( int argc, char* argv[] )
 
 	// Resample now
 	ResampleFilterType::Pointer resampledVolume = ResampleFilterType::New();
-	resampledVolume->SetInput( inputVolume->GetOutput() );
-	resampledVolume->SetTransform( transform );
-	resampledVolume->SetInterpolator( interpolator );
-	resampledVolume->SetOutputOrigin( inputAnnotatedVolume->GetOutput()->GetOrigin() );
-	resampledVolume->SetOutputSpacing( inputAnnotatedVolume->GetOutput()->GetSpacing() );
-	resampledVolume->SetOutputDirection( inputAnnotatedVolume->GetOutput()->GetDirection() );
-	resampledVolume->SetSize( inputAnnotatedVolume->GetOutput()->GetLargestPossibleRegion().GetSize() );
+	resampledVolume->SetInput(dce_volume->GetOutput());
+	resampledVolume->SetTransform(transform);
+	resampledVolume->SetInterpolator(interpolator);
+	resampledVolume->SetOutputOrigin(t2w_volume->GetOutput()->GetOrigin());
+	resampledVolume->SetOutputSpacing(t2w_volume->GetOutput()->GetSpacing());
+	resampledVolume->SetOutputDirection(t2w_volume->GetOutput()->GetDirection());
+	resampledVolume->SetSize(t2w_volume->GetOutput()->GetLargestPossibleRegion().GetSize());
 	resampledVolume->Update();
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	// Create a Metadata dictionary for the output. Only the origin and direction should be different.
-
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// CREATE A NEW DICTIONARY USING A MIXED OF T2W AND DCE INFORMATON
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
 	// For each slice create a dictionary
 	// Find the number of slice
 	InputImageType::SizeType outputSize = resampledVolume->GetOutput()->GetLargestPossibleRegion().GetSize();
@@ -223,46 +230,39 @@ int main( int argc, char* argv[] )
 	// Array where the different dictionaries will be saved
 	ReaderType::DictionaryArrayType outputArray;
 
-	for ( unsigned int nSlice = 0; nSlice < outputSize[2] ; nSlice++ ) {
-
+	for (unsigned int nSlice = 0; nSlice < outputSize[2]; ++nSlice) 
+	{
 	    // Prepare the type of the extractimagefilter to get statistic
-	    typedef itk::ExtractImageFilter< InputImageType, OutputImageType > ExtractFilterType;
 	    ExtractFilterType::Pointer filter3dto2d = ExtractFilterType::New();
-	    InputImageType::RegionType inputRegion = resampledVolume->GetOutput()->GetLargestPossibleRegion();
-	    InputImageType::SizeType size2d = inputRegion.GetSize();
-	    size2d[2] = 0;
-	    InputImageType::IndexType start2d = inputRegion.GetIndex();
-	    start2d[2] = nSlice;
-	    InputImageType::RegionType desiredRegion;
-	    desiredRegion.SetSize(  size2d  );
-	    desiredRegion.SetIndex( start2d );
+	    InputImageType::RegionType dce_slice_region = resampledVolume->GetOutput()->GetLargestPossibleRegion();
+	    InputImageType::SizeType dce_slice_size = dce_slice_region.GetSize();
+	    dce_slice_size[2] = 0;
+	    InputImageType::IndexType dce_slice_start_index = dce_slice_region.GetIndex();
+	    dce_slice_start_index[2] = nSlice;
+	    InputImageType::RegionType dce_roi;
+	    dce_roi.SetSize(dce_slice_size);
+	    dce_roi.SetIndex(dce_slice_start_index);
 	    filter3dto2d->SetDirectionCollapseToIdentity();
-	    filter3dto2d->SetInput( resampledVolume->GetOutput() );
-	    filter3dto2d->SetExtractionRegion( desiredRegion );
+	    filter3dto2d->SetInput(resampledVolume->GetOutput());
+	    filter3dto2d->SetExtractionRegion(dce_roi);
 	    filter3dto2d->Update();
-	    
-	    typedef itk::StatisticsImageFilter< OutputImageType > StatisticsImageFilterType;
-	    StatisticsImageFilterType::Pointer statisticsImageFilter = StatisticsImageFilterType::New();
-	    statisticsImageFilter->SetInput(filter3dto2d->GetOutput());
-	    statisticsImageFilter->Update();
-
-	    std::cout << "Mean: " << statisticsImageFilter->GetMean() << std::endl;
-	    std::cout << "Std.: " << statisticsImageFilter->GetSigma() << std::endl;
-	    std::cout << "Min: " << statisticsImageFilter->GetMinimum() << std::endl;
-	    std::cout << "Max: " << statisticsImageFilter->GetMaximum() << std::endl;
+	    	    
+	    StatisticsImageFilterType::Pointer statistic_dce_slice = StatisticsImageFilterType::New();
+	    statistic_dce_slice->SetInput(filter3dto2d->GetOutput());
+	    statistic_dce_slice->Update();
 
 	    // Get the dictionary of the input slice 
-	    ReaderType::DictionaryRawPointer inputDict = (*(inputVolume->GetMetaDataDictionaryArray()))[0];
+	    ReaderType::DictionaryRawPointer dce_dict = (*(dce_volume->GetMetaDataDictionaryArray()))[0];
 
 	    // Get the dictionary of the T2W slice
-	    ReaderType::DictionaryRawPointer inputT2WDict = 
-		(*(inputAnnotatedVolume->GetMetaDataDictionaryArray()))[nSlice];
+	    ReaderType::DictionaryRawPointer t2w_dict = 
+		(*(t2w_volume->GetMetaDataDictionaryArray()))[nSlice];
     
 	    // Create a the output dictionary
-	    ReaderType::DictionaryRawPointer outputDict = new ReaderType::DictionaryType;
+	    ReaderType::DictionaryRawPointer out_dict = new ReaderType::DictionaryType;
 
 	    // Copy the input dictionary to the output dictionary
-	    CopyDictionary( *inputDict, *outputDict );
+	    CopyDictionary(*dce_dict, *out_dict);
 
 	    // String that will be used to change each necessary field of the output dictionary
 	    itksys_ios::ostringstream value;
@@ -276,24 +276,24 @@ int main( int argc, char* argv[] )
 
 	    // Patient Name
 	    value.str("Anonym^Patient");
-	    itk::EncapsulateMetaData<std::string>(*outputDict,"0010|0010", value.str());
+	    itk::EncapsulateMetaData<std::string>(*out_dict,"0010|0010", value.str());
 
 	    // SOP Instance UID
 	    gdcm::UIDGenerator sopuid;
 	    std::string sopInstanceUID = sopuid.Generate();
-	    itk::EncapsulateMetaData<std::string>(*outputDict,"0008|0018", sopInstanceUID);
+	    itk::EncapsulateMetaData<std::string>(*out_dict,"0008|0018", sopInstanceUID);
 
 	    // Attending Physician's Name
 	    value.str("Anonym^Physician");
-	    itk::EncapsulateMetaData<std::string>(*outputDict,"0008|1050", value.str());
+	    itk::EncapsulateMetaData<std::string>(*out_dict,"0008|1050", value.str());
 
 	    // Smallest Image Pixel Value
-	    std::string s_pix_val = statisticsImageFilter->GetMinimum();
-	    itk::EncapsulateMetaData<std::string>(*outputDict,"0028|0106", s_pix_val);
+	    std::string s_pix_val = std::to_string(statistic_dce_slice->GetMinimum());
+	    itk::EncapsulateMetaData<std::string>(*out_dict,"0028|0106", s_pix_val);
 
 	    // Largest Image Pixel Value
-	    std::string l_pix_val = statisticsImageFilter->GetMaximum();
-	    itk::EncapsulateMetaData<std::string>(*outputDict,"0018|0107", l_pix_val);
+	    std::string l_pix_val = std::to_string(statistic_dce_slice->GetMaximum());
+	    itk::EncapsulateMetaData<std::string>(*out_dict,"0028|0107", l_pix_val);
 
 	    // We need to keep the following element from the T2W serie
 	    // - (0020|0013) - Image Number
@@ -310,45 +310,42 @@ int main( int argc, char* argv[] )
 	    // Image Number
 	    value.str("");
 	    value << nSlice + 1;
-	    itk::EncapsulateMetaData<std::string>(*outputDict,"0020|0013", value.str());
+	    itk::EncapsulateMetaData<std::string>(*out_dict,"0020|0013", value.str());
 
 	    // Create an vector with all the tag to recopy
-	    std::vector<std::string> tags_t2w_out = { "0020|0032" , "0020|0037)", "0020|0052", "0020|1041", 
-						      "0028|0010", "0028|0011", "0028|0030", "0028|1050", 
-						      "0028|1051"};
+	    std::vector<std::string> tags_t2w_out = {"0020|0032" , "0020|0037)", "0020|0052", "0020|1041", 
+						     "0028|0010", "0028|0011", "0028|0030", "0028|1050", 
+						     "0028|1051"};
 
 	    for(auto it = tags_t2w_out.begin(); it != tags_t2w_out.end(); ++it) {
 		std::string val_dict;
 		// Get from the dictionnary of T2W
-		itk::ExposeMetaData<std::string>(*inputT2WDict, *it, val_dict);
+		itk::ExposeMetaData<std::string>(*t2w_dict, *it, val_dict);
 		// Copy to the output dictionnary
-		itk::EncapsulateMetaData<std::string>(*outputDict, *it, val_dict);
+		itk::EncapsulateMetaData<std::string>(*out_dict, *it, val_dict);
 	    }
 
-	    std::string val_dict;
-	    itk::ExposeMetaData<std::string>(*inputT2WDict, "0020|000e", val_dict);
-	    std::cout << val_dict << std::endl;
-
 	    // Save the dictionary
-	    outputArray.push_back(outputDict);
+	    outputArray.push_back(out_dict);
 	}
 
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// SAVE THE DATA AS SLICES 
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	//////////////////////////////////////////////////////////////
-	// Let's save the data again with GDCM standard orientation
 
 	ResampleFilterType::Pointer resampledVolume2 = ResampleFilterType::New();
-	resampledVolume2->SetInput( inputVolume->GetOutput() );
-	resampledVolume2->SetTransform( transform );
-	resampledVolume2->SetInterpolator( interpolator );
-	resampledVolume2->SetOutputOrigin( inputAnnotatedVolume->GetOutput()->GetOrigin() );
-	resampledVolume2->SetOutputSpacing( inputAnnotatedVolume->GetOutput()->GetSpacing() );
-	resampledVolume2->SetOutputDirection( inputAnnotatedVolume->GetOutput()->GetDirection() );
-	resampledVolume2->SetSize( inputAnnotatedVolume->GetOutput()->GetLargestPossibleRegion().GetSize() );
+	resampledVolume2->SetInput(dce_volume->GetOutput());
+	resampledVolume2->SetTransform(transform);
+	resampledVolume2->SetInterpolator(interpolator);
+	resampledVolume2->SetOutputOrigin(t2w_volume->GetOutput()->GetOrigin());
+	resampledVolume2->SetOutputSpacing(t2w_volume->GetOutput()->GetSpacing());
+	resampledVolume2->SetOutputDirection(t2w_volume->GetOutput()->GetDirection());
+	resampledVolume2->SetSize(t2w_volume->GetOutput()->GetLargestPossibleRegion().GetSize());
 	resampledVolume2->Update();
  
 	// Make the output directory and generate the file names.
-	itksys::SystemTools::MakeDirectory( argv[3] );
+	itksys::SystemTools::MakeDirectory(argv[3]);
 
 	// Generate the file names
 	OutputNamesGeneratorType::Pointer outputNames = OutputNamesGeneratorType::New();
@@ -359,11 +356,12 @@ int main( int argc, char* argv[] )
 	outputNames->SetEndIndex (nFile+outputSize[2]);
 
 	SeriesWriterType::Pointer seriesWriter = SeriesWriterType::New();
-	seriesWriter->SetInput( resampledVolume2->GetOutput() );
-	gdcmIO->KeepOriginalUIDOn();
-	seriesWriter->SetImageIO( gdcmIO );
-	seriesWriter->SetFileNames( outputNames->GetFileNames() );
-	seriesWriter->SetMetaDataDictionaryArray( &outputArray );
+	seriesWriter->SetInput(resampledVolume2->GetOutput());
+	// To not generate any new UID
+	gdcm_t2w->KeepOriginalUIDOn();
+	seriesWriter->SetImageIO(gdcm_t2w);
+	seriesWriter->SetFileNames(outputNames->GetFileNames());
+	seriesWriter->SetMetaDataDictionaryArray(&outputArray);
 	try {
 	    seriesWriter->Update();
 	}
