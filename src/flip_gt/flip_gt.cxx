9e7f45970116f0e7db7ffbe190ce29c2f4ee3b63
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-04-04 17:13:55 +0200

    Add the flip in C++

---
+++ src/flip_gt/flip_gt.cxx
@@ -0,0 +1,77 @@
+#include "itkGDCMImageIO.h"
+#include "itkGDCMSeriesFileNames.h"
+#include "itkImageSeriesReader.h"
+#include "itkImageSeriesWriter.h"
+#include <vector>
+#include "itksys/SystemTools.hxx"
+int main( int argc, char* argv[] )
+{
+    if( argc < 3 )
+    {
+	std::cerr << "Usage: " << argv[0] <<
+	    " DicomDirectory  OutputDicomDirectory" << std::endl;
+	return EXIT_FAILURE;
+    }
+
+    typedef signed short    PixelType;
+    const unsigned int      Dimension = 3;
+    typedef itk::Image< PixelType, Dimension >      ImageType;
+    typedef itk::ImageSeriesReader< ImageType >     ReaderType;
+    typedef itk::GDCMImageIO                        ImageIOType;
+    typedef itk::GDCMSeriesFileNames                NamesGeneratorType;
+
+    ImageIOType::Pointer gdcmIO = ImageIOType::New();
+
+    NamesGeneratorType::Pointer namesGenerator = NamesGeneratorType::New();
+    namesGenerator->SetInputDirectory( argv[1] );
+    const ReaderType::FileNamesContainer & filenames =
+	namesGenerator->GetInputFileNames();
+    unsigned int numberOfFilenames =  filenames.size();
+    std::cout << numberOfFilenames << std::endl;
+
+    for(unsigned int fni = 0; fni<numberOfFilenames; fni++)
+    {
+	std::cout << "filename # " << fni << " = ";
+	std::cout << filenames[fni] << std::endl;
+    }
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetImageIO( gdcmIO );
+    reader->SetFileNames( filenames );
+
+  try
+  {
+      reader->Update();
+  }
+  catch (itk::ExceptionObject &excp)
+  {
+      std::cerr << "Exception thrown while writing the image" << std::endl;
+      std::cerr << excp << std::endl;
+      return EXIT_FAILURE;
+  }
+
+  const char * outputDirectory = argv[2];
+  itksys::SystemTools::MakeDirectory( outputDirectory );
+  typedef signed short    OutputPixelType;
+  const unsigned int      OutputDimension = 2;
+  typedef itk::Image< OutputPixelType, OutputDimension >    Image2DType;
+  typedef itk::ImageSeriesWriter<
+      ImageType, Image2DType >  SeriesWriterType;
+  SeriesWriterType::Pointer seriesWriter = SeriesWriterType::New();
+  seriesWriter->SetInput( reader->GetOutput() );
+  seriesWriter->SetImageIO( gdcmIO );
+  namesGenerator->SetOutputDirectory( outputDirectory );
+  seriesWriter->SetFileNames( namesGenerator->GetOutputFileNames() );
+  seriesWriter->SetMetaDataDictionaryArray(
+      reader->GetMetaDataDictionaryArray() );
+  try
+  {
+      seriesWriter->Update();
+  }
+  catch( itk::ExceptionObject & excp )
+  {
+      std::cerr << "Exception thrown while writing the series " << std::endl;
+      std::cerr << excp << std::endl;
+      return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
